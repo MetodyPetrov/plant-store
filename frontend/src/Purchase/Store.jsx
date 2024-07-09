127f588830bcb76c3ed19425a2e7402bcd9f9c2f
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-10 01:35:23 +0300

    search bar implementation

--- frontend/src/Purchase/Store.jsx
+++ frontend/src/Purchase/Store.jsx
@@ -4,6 +4,7 @@ import { fetchOffers } from '../utils/http';
 import ProgressDialog from '../Dialogs/ProgressDialog';
 import Offer from './Offer';
 import AddOffer from '../childComponents/AddOffer';
+import { useEffect, useState } from 'react';
 
 export default function Store() {
     const { data, isPending } = useQuery({
@@ -11,19 +12,43 @@ export default function Store() {
         queryFn: fetchOffers
     });
 
+    const [ offers, setOffers ] = useState(null);
+    const [ search, setSearch ] = useState(null);
+
+    useEffect(() => {
+        setOffers(data);
+    }, [data]);
+
+    function handleSearch(e) {
+        if(!e.target.value) {
+            setOffers(data);
+            setSearch('');
+        }
+        else {
+            setSearch(e.target.value);
+            setOffers(() => {
+                if(isNaN(e.target.value)) return data.filter(offer => offer.name.toLowerCase().includes(e.target.value.toLowerCase()));
+                else return data.filter(offer => offer.price.includes(e.target.value));
+            });
+        }
+    }
+    
     return (
         <>
             <div className="page-bg storepage-bg"></div>
+            <div className="search-bar-wrapper">
+                <input className="search-bar" placeholder="Search Product" onChange={handleSearch}></input>
+            </div>
             {
             isPending ? <ProgressDialog><h1>Loading Offers...</h1></ProgressDialog> :
 
-            (data &&
+            (offers &&
             <>
                 <div className="offers-container-wrapper">
                     <div className="offers-container">
                         {localStorage.getItem('client') === 'admin' && <AddOffer />}
-                        {data.map((offer, index) => (
-                            <Offer key={index} offer={offer}/>
+                        {offers.map((offer, index) => (
+                            <Offer key={index} offer={offer} searchQuery={search}/>
                         ))}
                     </div>
                 </div>
