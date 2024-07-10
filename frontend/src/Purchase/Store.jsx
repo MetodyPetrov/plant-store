ba6b8e36bc7ef5c56adea335f0e720fdcc0ed75b
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-11 01:47:42 +0300

    added search by type

--- frontend/src/Purchase/Store.jsx
+++ frontend/src/Purchase/Store.jsx
@@ -7,19 +7,25 @@ import AddOffer from '../childComponents/AddOffer';
 import { useEffect, useState } from 'react';
 
 export default function Store() {
-    const { data, isPending } = useQuery({
+    const { data, isPending, refetch } = useQuery({
         queryKey: ['offers'],
         queryFn: fetchOffers
     });
 
     const [ offers, setOffers ] = useState(null);
     const [ search, setSearch ] = useState(null);
+    const [ typeSearch, setTypeSearch ] = useState(false);
 
     useEffect(() => {
         setOffers(data);
     }, [data]);
 
+    useEffect(() => {
+        handleSearch('mode');
+    }, [typeSearch]);
+
     function handleSearch(e) {
+        if(e === 'mode') e = { target: { value: search } };
         if(!e.target.value) {
             setOffers(data);
             setSearch('');
@@ -27,8 +33,13 @@ export default function Store() {
         else {
             setSearch(e.target.value);
             setOffers(() => {
-                if(isNaN(e.target.value)) return data.filter(offer => offer.name.toLowerCase().includes(e.target.value.toLowerCase()));
-                else return data.filter(offer => offer.price.includes(e.target.value));
+                let originalOffers = [...data];
+                if(isNaN(e.target.value)) {
+                    if(typeSearch) originalOffers = originalOffers.filter(offer => offer.type.toLowerCase().includes(e.target.value.toLowerCase()));
+                    else originalOffers = originalOffers.filter(offer => offer.name.toLowerCase().includes(e.target.value.toLowerCase()));
+                }
+                else originalOffers = originalOffers.filter(offer => offer.price.includes(e.target.value));
+                return originalOffers;
             });
         }
     }
@@ -38,6 +49,10 @@ export default function Store() {
             <div className="page-bg storepage-bg"></div>
             <div className="search-bar-wrapper">
                 <input className="search-bar" placeholder="Search Product" onChange={handleSearch}></input>
+                <div className="type-search">
+                    <input type="radio" id="type" readOnly checked={typeSearch} onClick={() => setTypeSearch((prev) => !prev)}></input>
+                    <label htmlFor="type">type</label>
+                </div>
             </div>
             {
             isPending ? <ProgressDialog><h1>Loading Offers...</h1></ProgressDialog> :
@@ -46,9 +61,9 @@ export default function Store() {
             <>
                 <div className="offers-container-wrapper">
                     <div className="offers-container">
-                        {localStorage.getItem('client') === 'admin' && <AddOffer />}
+                        {localStorage.getItem('client') === 'admin' && <AddOffer displayType={typeSearch}/>}
                         {offers.map((offer, index) => (
-                            <Offer key={index} offer={offer} searchQuery={search}/>
+                            <Offer key={index} offer={offer} searchQuery={search} displayType={typeSearch} refetchOffers={refetch}/>
                         ))}
                     </div>
                 </div>
