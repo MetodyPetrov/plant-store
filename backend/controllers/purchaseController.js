fe2b4b6538f4418618abd1d80ccdd6be63cedaa4
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-11 01:41:44 +0300

    fixed withTransaction

--- backend/controllers/purchaseController.js
+++ backend/controllers/purchaseController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { getCollection, client } = require('../utils/utils');
-const { ObjectId } = require('mongodb');
+const { ObjectId, Int32 } = require('mongodb');
 
 const { isAuth } = require('../middlewares/auth');
 
@@ -16,38 +16,34 @@ router.post('/buy', async (req, res) => {
     const session = client.startSession();
     try {
         const transactionResults = await session.withTransaction(async () => {
-            return new Promise(async (resolve, reject) => {
-                for (const purchase of req.body) {
-                    const account = await accountsCollection.findOne({ username: req.user.username }, {session} );
-                    const product = await offersCollection.findOne({ _id: new ObjectId(purchase.id) }, { session });
-    
-                    product.price = parseFloat(product.price, 10).toFixed(2) * 100;
-    
-                    if(product.quantity < purchase.amount) reject('Instock product quantity is not enough.');
-                    if(account.credit - purchase.amount * product.price < 0) reject('Insufficient funds.');
-                    await accountsCollection.updateOne(
-                        { username: req.user.username },
-                        { $inc: { credit: -(purchase.amount * product.price) } },
-                        { session }
-                    );
-                    //console.log(`Reduced credit of user ${req.user.username} by ${purchase.amount * product.price}`);
-                    
-                    let currentResult = await offersCollection.updateOne(
-                        { _id: new ObjectId(purchase.id) },
-                        { $inc: { quantity: -purchase.amount } },
-                        { session }
-                    );
-                    //console.log(`Reduced the quanitity of product with id: ${purchase.id}, by ${currentResult.matchedCount}`);
-    
-                    currentResult = await historyCollection.insertOne({ account: req.user.username, product: product.name, amount: purchase.amount }, { session });
-                }
-                resolve('Successful purchase!');
-            });
+            for (const purchase of req.body) {
+                const account = await accountsCollection.findOne({ username: req.user.username }, {session} );
+                const product = await offersCollection.findOne({ _id: new ObjectId(purchase.id) }, { session });
+
+                product.price = parseFloat(product.price, 10).toFixed(2) * 100;
+
+                if(product.quantity < purchase.amount) throw new Error('Instock product quantity is not enough.');
+                if(account.credit - purchase.amount * product.price < 0) throw new Error('Insufficient funds.');
+                await accountsCollection.updateOne(
+                    { username: req.user.username },
+                    { $inc: { credit: -(purchase.amount * product.price) } },
+                    { session }
+                );
+                
+                let currentResult = await offersCollection.updateOne(
+                    { _id: new ObjectId(purchase.id) },
+                    { $inc: { quantity: -purchase.amount } },
+                    { session }
+                );
+
+                currentResult = await historyCollection.insertOne({ account: req.user.username, product: product.name, amount: new Int32(purchase.amount) }, { session });
+            }
+            return 'Successful purchase!';
         });
         res.status(200).json({ message: transactionResults });
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
+        console.error(err.message);
+        res.status(500).json({ message: err.message });
     } finally {
         session.endSession();
     }
