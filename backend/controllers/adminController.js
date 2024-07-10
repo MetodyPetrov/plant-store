823fccaa119f8ba2f24681d89851c4788f064de6
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-11 01:40:30 +0300

    fixed history product amount mongodb type

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 const { getCollection } = require('../utils/utils');
 
-const { ObjectId, Int32 } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const { isAuth } = require('../middlewares/auth');
 const { isAdmin } = require('../middlewares/admin');
 
 router.use(isAuth);
 router.use(isAdmin);
 
-router.put('/credit', isAuth, async (req, res) => {
+router.put('/credit', async (req, res) => {
     try {
         const collection = await getCollection('accounts');
         const account = await collection.updateOne(
@@ -17,10 +17,37 @@ router.put('/credit', isAuth, async (req, res) => {
             { $inc: { credit: req.body.credit * 1 } }
         );
         
-        res.status(200).json({ message: `Successfuly increased credit by ${req.body.credit}` });
+        res.status(200).json({ message: `Successfuly increased credit by ${parseFloat(req.body.credit / 100).toFixed(2)}` });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.put('/offers/:id/increase-quantity', async (req, res) => {
+    try {
+        const collection = await getCollection('offers');
+        const result = await collection.updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $inc: { quantity: req.body.quantity * 1 } }
+        );
+        if(result.modifiedCount) return res.status(200).json({ message: 'Successful increase' });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Unsuccessful increase' });
+    }
+});
+router.put('/offers/:id/decrease-quantity', async (req, res) => {
+    try {
+        const collection = await getCollection('offers');
+        const result = await collection.updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $inc: { quantity: -req.body.quantity * 1 } }
+        );
+        if(result.modifiedCount) return res.status(200).json({ message: 'Successful decrease' });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Unsuccessful decrease' });
     }
 });
 
