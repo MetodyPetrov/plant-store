e46f315c2a0e28e3637cf83450e6d1d9af5fda5e
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-11 01:41:15 +0300

    made it so that only if an admin is logged all offers are shown (even those without any quantity)

--- backend/controllers/offersController.js
+++ backend/controllers/offersController.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { getCollection } = require('../utils/utils');
-const { ObjectId } = require('mongodb');
 
 router.get('/', async (req, res) => {
     try {
         const collection = await getCollection('offers');
         const offers = await collection.find().toArray();
-        res.status(200).json(offers);
+        if(req.user?.client === 'admin') res.status(200).json(offers);
+        else res.status(200).json(offers.filter(offer => offer.quantity > 0));
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Internal server error' });
