f7552c3baa009f334d811b180bc85f48ce1e1ddb
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-11 01:47:03 +0300

    admin options, offers refetching after admin offer quanity change, type display on search by type, function outsourcing for highlighting search matches

--- frontend/src/Purchase/Offer.jsx
+++ frontend/src/Purchase/Offer.jsx
@@ -5,15 +5,18 @@ import Quantity from "../childComponents/Quantity";
 import OfferDescription from "../childComponents/OfferDescription";
 import CartIcon from "../childComponents/CartIcon";
 import { CartContext } from "./CartContext";
+import AdminOptions from "../childComponents/AdminOptions";
 
-export default function Offer({ offer, searchQuery }) {
+export default function Offer({ offer, searchQuery, displayType = false, refetchOffers }) {
     const { addItemToCart } = useContext(CartContext);
 
     const inputQuantity = useRef(null);
     const [ viewDescription, setViewDescription ] = useState(false);
+    const [ viewAdminOptions, setViewAdminOptions ] = useState(false);
     
     const [ offerTitle, setOfferTitle] = useState(offer?.name);
     const [ offerPrice, setOfferPrice ] = useState(offer?.price);
+    const [ offerType, setOfferType ] = useState(offer?.type);
 
     const { changeNavBar, checkNavBarPageExist } = useOutletContext();
     function handleAddProduct() {
@@ -21,36 +24,52 @@ export default function Offer({ offer, searchQuery }) {
         if(!checkNavBarPageExist(<CartIcon className="cart-icon"/>)) changeNavBar({ mode: 'ADD', newKey: <CartIcon className="cart-icon"/>, newValue: '/cart' }); // TODO: make sure to add this only once and think about storing the selected items in some context, not sure if it should be the outlet one or no...
     }
 
+    function handleRightClick(e) {
+        if(localStorage.getItem('client') !== 'admin') return;
+        e.preventDefault();
+        setViewAdminOptions(<AdminOptions offerId={offer._id} unmount={() => setViewAdminOptions(false)} pos={e} refetchOffers={refetchOffers}/>); // make this into a separate component and add a + and - that sent req for increasing the value of a product
+    }
+
     useEffect(() => {
+        function highlightSearch(attribute, stateChange) { 
+            let newTranscript = [];
+            for (let i = 0; i < attribute.length; i++) {
+                let part = '';
+                for (let j = 0; j < searchQuery.length && j + i < attribute.length; j++) {
+                    part += attribute[j + i];
+                }
+                if(part.toLowerCase() === searchQuery.toLowerCase()) {
+                    i += searchQuery.length - 1;
+                    newTranscript.push(<span className="highlighted" key={i}>{part}</span>);
+                }
+                else newTranscript.push(attribute[i]);
+            }
+            stateChange(newTranscript);
+        }
+
         if(!searchQuery) {
             setOfferTitle(offer?.name);
             setOfferPrice(offer?.price);
+            setOfferType(offer?.type);
             return;
         }
-        const isnumber = !isNaN(searchQuery);
-        let newTranscript = [];
-        for (let i = 0; i < (isnumber ? offer.price.length : offer.name.length); i++) {
-            let part = '';
-            for (let j = 0; j < searchQuery.length && j + i < (isnumber ? offer.price.length : offer.name.length); j++) {
-                part += (isnumber ? offer.price[j + i] : offer.name[j + i]);
-            }
-            if(part.toLowerCase() === searchQuery.toLowerCase()) {
-                i += searchQuery.length - 1;
-                newTranscript.push(<span className="highlighted" key={i}>{part}</span>);
-            }
-            else newTranscript.push((isnumber ? offer.price[i] : offer.name[i])); // TODO: outsource this into a function and maybe think about how to sort by type
-        }
-        isnumber ? setOfferPrice(newTranscript) : setOfferTitle(newTranscript);
+
+        if(displayType) highlightSearch(offer?.type, setOfferType);
+        else if(isNaN(searchQuery)) highlightSearch(offer?.name, setOfferTitle);
+        else highlightSearch(offer?.price, setOfferPrice);
+        
     }, [searchQuery, offer.name, offer.price]);
 
     return (
         <div className="offer">
-            <Link to={`/store/${offer?._id}`}><img src={offer?.url} alt={offer?.name} className="offer-img"></img></Link>
+            { viewAdminOptions }
+            <Link to={`/store/${offer?._id}`}><img src={offer?.url} alt={offer?.name} className="offer-img" onContextMenu={handleRightClick}></img></Link>
             <br/>
             <h1 className="offer-title" onClick={(e) => setViewDescription(e)}>{offerTitle || 'Unset Title'}</h1>
 
             { viewDescription && <OfferDescription pos={viewDescription} unmount={() => setViewDescription(false)}>{offer?.description || 'unset description'}</OfferDescription>}
             <div className="flex-filler"></div>
+            { displayType && <p className="offer-type">Type: {offerType}</p> }
             <div className="offer-info">
                 <h2 className="offer-price">{offerPrice || '0.00 lv'}</h2>
                 <Quantity quantity={offer?.quantity || 0} inpRef={inputQuantity}/>
