ddb829a70dfdf1706eabbf910fe34c4a8d9d0c35
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-13 02:33:56 +0300

    max quantity changability for when a new offer is being created

--- frontend/src/childComponents/Quantity.jsx
+++ frontend/src/childComponents/Quantity.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
-export default function Quantity({ quantity = 0, inpRef }) {
+export default function Quantity({ quantity = 0, inpRef, editMaxQuantity = false }) {
     const [ amount, setAmount ] = useState(1);
     return (
         <div className="quantity-selector">
-            <span className="quantity-change" onClick={() => inpRef && inpRef.current.stepDown()}>-</span>
-            <input type="number" min="1" max={quantity} ref={inpRef} value={amount} onChange={(e) => e.target.value <= quantity * 1 && setAmount(e.target.value)}></input>
-            <span className="max-quantity">/{quantity}</span>
-            <span className="quantity-change" onClick={() => inpRef && inpRef.current.stepUp()}>+</span>
+            <span className="quantity-change" onClick={() => inpRef && inpRef.current.stepDown()} style={editMaxQuantity ? {cursor: 'not-allowed'} : {}}>-</span>
+            <input type="number" className="input-quantity" min="1" max={quantity} ref={editMaxQuantity ? undefined : inpRef} value={amount} onChange={(e) => e.target.value <= quantity * 1 && setAmount(e.target.value)} disabled={editMaxQuantity}></input>
+            { editMaxQuantity ? 
+            <input className="max-quantity admin" contentEditable={editMaxQuantity} suppressContentEditableWarning={true} style={editMaxQuantity ? {cursor: 'pointer'} : {}} ref={editMaxQuantity ? inpRef : undefined} defaultValue={quantity}></input>: 
+            <span className="max-quantity" contentEditable={editMaxQuantity} suppressContentEditableWarning={true} style={editMaxQuantity ? {cursor: 'pointer'} : {}} ref={editMaxQuantity ? inpRef : undefined}>/{quantity}</span>
+            }
+            <span className="quantity-change" onClick={() => inpRef && inpRef.current.stepUp()} style={editMaxQuantity ? {cursor: 'not-allowed'} : {}}>+</span>
         </div>
     );
 }
