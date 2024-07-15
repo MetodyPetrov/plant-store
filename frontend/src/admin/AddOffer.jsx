a20486c072b149546bf06fbe443132bd6827aee4
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-15 03:07:31 +0300

    small bug fixes & offers refetching upon offer creation

--- frontend/src/admin/AddOffer.jsx
+++ frontend/src/admin/AddOffer.jsx
@@ -6,7 +6,7 @@ import Modal from "../Dialogs/Modal";
 import Offer from "../Purchase/Offer";
 import { fetchAddOffer } from "../utils/http";
 
-export default function AddOffer({ displayType = false }) {
+export default function AddOffer({ reloadOffers }) {
     const [ imageAdd, setImageAdd ] = useState(false);
     const [ viewDescription, setViewDescription ] = useState(false);
     const [ previewOffer, setPreviewOffer ] = useState(false);
@@ -18,9 +18,11 @@ export default function AddOffer({ displayType = false }) {
     const [ price, setPrice ] = useState();
     const [ imgUrl, setImgUrl ] = useState();
 
-    function handleAddNewOffer(e) {
+    async function handleAddNewOffer(e) {
         if(e === 'confirmed') {
-            fetchAddOffer({ description: description, name: title, price: price, quantity: maxQuantity.current.value, type: type, url: imgUrl });
+            await fetchAddOffer({ description: description, name: title, price: price, quantity: maxQuantity.current.value, type: type, url: imgUrl }) &&
+                setPreviewOffer(false) && 
+                    await reloadOffers();
         } else {
             e.preventDefault();
             setPreviewOffer(true);
@@ -37,9 +39,9 @@ export default function AddOffer({ displayType = false }) {
             </Modal> }
 
             { previewOffer && <Modal unmount={() => setPreviewOffer(false)}>
-                <form>
+                <form onSubmit={(e) => { e.preventDefault(); handleAddNewOffer('confirmed'); }}>
                     <Offer offer={{ description: description, name: title, price: price, quantity: maxQuantity.current.value, type: type, url: imgUrl }} displayType={true} adminOptions={false} className={"preview-offer"}/>
-                    <button className="add-offer-confirm" onClick={() => handleAddNewOffer('confirmed')}>Confirm</button>
+                    <button className="add-offer-confirm">Confirm</button>
                 </form>
             </Modal> }
 
@@ -50,10 +52,11 @@ export default function AddOffer({ displayType = false }) {
 
                 { viewDescription && <OfferDescription pos={viewDescription} unmount={() => setViewDescription(false)} classes="add-offer-description" description={setDescription}>Set Description</OfferDescription>}
                 <div className="flex-filler"></div>
-                { displayType && <p className="offer-type" contentEditable={true} suppressContentEditableWarning={true} onInput={(e) => setType(e.target.innerText)}>Set Type</p> }
-                <button className="admin-plus-button" style={{border: 'solid black 1px'}}><span>+</span></button>
+                <p className="offer-type" contentEditable={true} suppressContentEditableWarning={true} onInput={(e) => setType(e.target.innerText)}>Set Type</p>
+                <br/>
+                <button className="admin-plus-button offer-create-button"><span>+</span></button>
                 <div className="offer-info">
-                    <h2 className="offer-price" contentEditable={true} suppressContentEditableWarning={true} spellCheck={false} onInput={(e) => setPrice(e.target.innerText)}>0.00 lv</h2>
+                    <h2 className="offer-price" contentEditable={true} suppressContentEditableWarning={true} spellCheck={false} onInput={(e) => setPrice(e.target.innerText)}>0.00 lv.</h2>
                     <Quantity inpRef={maxQuantity} editMaxQuantity={true}/>
                     <svg className="add-cart-icon" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#5f6368"><path d="M284.53-80.67q-30.86 0-52.7-21.97Q210-124.62 210-155.47q0-30.86 21.98-52.7Q253.95-230 284.81-230t52.69 21.98q21.83 21.97 21.83 52.83t-21.97 52.69q-21.98 21.83-52.83 21.83Zm400 0q-30.86 0-52.7-21.97Q610-124.62 610-155.47q0-30.86 21.98-52.7Q653.95-230 684.81-230t52.69 21.98q21.83 21.97 21.83 52.83t-21.97 52.69q-21.98 21.83-52.83 21.83ZM238.67-734 344-515.33h285.33l120-218.67H238.67ZM206-800.67h589.38q22.98 0 34.97 20.84 11.98 20.83.32 41.83L693.33-490.67q-11 19.34-28.87 30.67-17.87 11.33-39.13 11.33H324l-52 96h487.33V-286H278q-43 0-63-31.83-20-31.84-.33-68.17l60.66-111.33-149.33-316H47.33V-880h121.34L206-800.67Zm138 285.34h285.33H344Z"/></svg>
                 </div>
