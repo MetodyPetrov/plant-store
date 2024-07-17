7d0b3a5638ef19e04f4f307f18b60783ada70181
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-17 04:31:00 +0300

    helperFunctions and offer validation

---
+++ backend/utils/helperFunctions.js
@@ -0,0 +1,40 @@
+const { getCollection } = require('./utils');
+
+async function validateOfferInfo(offerInfo, mode) {
+    const collection = await getCollection('offers');
+    let offer;
+    if(mode === 'full')  {
+        offer = {
+            name: offerInfo.name,
+            price: offerInfo.price,
+            quantity: new Int32(offerInfo.quantity),
+            type: offerInfo.type,
+            url: offerInfo.url,
+            description: offerInfo.description
+        }
+    } else {
+        offer = offerInfo;
+    }
+
+    // IMPORTANT: Image validation must be part of the frontend
+
+    for (const key in offer) {
+        if (offer.hasOwnProperty(key)) {
+            if(!offer[key]) throw new Error(`Undefined offer property: ${key}`);
+        }
+    }
+
+    const validatePrice = /^\d{2}\.\d{2} lv\.$/;
+
+    if(offer.quantity && isNaN(offer.quantity)) throw new Error('Invalid offer quantity');
+    if(offer.price && (isNaN(parseFloat(offer.price, 10).toFixed(2)) || !validatePrice.test(offer.price))) throw new Error('Invalid offer price');
+    if(offer.description && typeof offer.description !== 'string') throw new Error('Invalid offer description');
+
+    if(offer.name) {
+        const existingOffer = await collection.findOne({ name: offer.name });
+        if(existingOffer) throw new Error('Offer already exists');
+    }
+    return true;
+}
+
+module.exports = { validateOfferInfo };
