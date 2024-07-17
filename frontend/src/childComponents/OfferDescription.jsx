04df62d26353bed798313c5055680d6fc7b14ec6
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-17 04:20:03 +0300

    element positioning prop

--- frontend/src/childComponents/OfferDescription.jsx
+++ frontend/src/childComponents/OfferDescription.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-export default function OfferDescription({ pos, children, unmount, classes = '', description }) {
+export default function OfferDescription({ pos, children, unmount, classes = '', description, absolute = true }) {
     const descriptionRef = useRef(null);
 
     useEffect(() => {
@@ -11,11 +11,14 @@ export default function OfferDescription({ pos, children, unmount, classes = '',
         };
     }, [unmount, pos.target]);
 
+    const styling = {};
+    if(absolute) {
+        styling.position = 'absolute';
+        styling.left = pos.clientX + window.scrollX;
+        styling.top = pos.clientY + window.scrollY;
+    }
+
     return (
-        <p style={{
-            position: 'absolute',
-            left: pos.clientX + window.scrollX,
-            top: pos.clientY + window.scrollY
-        }} className={"offer-description " + classes} ref={descriptionRef} contentEditable={description && true} suppressContentEditableWarning={description && true} onInput={(e) => description(e.target.innerText)}>{children}</p>
+        <p style={styling} className={"offer-description " + classes} ref={descriptionRef} contentEditable={description && true} suppressContentEditableWarning={description && true} onInput={(e) => description(e.target.innerText)}>{children}</p>
     );
 }
