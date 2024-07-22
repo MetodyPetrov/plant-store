fd363b1bd3f9877bd7bc2e7e53070367f2de5279
MetodyPetrov <metody.p.petrov@gmail.com>
2024-07-23 01:44:20 +0300

    format number to 00.00 function

--- frontend/src/utils/functions.js
+++ frontend/src/utils/functions.js
@@ -6,3 +6,11 @@ export function isValidImageUrl(url) {
       img.src = url;
     });
 }
+
+export function formatNumber(num) {
+  let [firstPart, secondPart] = parseFloat(num).toFixed(2).split('.');
+  if (firstPart.length < 2) {
+    firstPart = '0' + firstPart;
+  }
+  return `${firstPart}.${secondPart}`;
+}
