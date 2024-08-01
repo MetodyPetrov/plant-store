b267edba19a7a3f4bd3abc1f624c8790519e025e
MetodyPetrov <metody.p.petrov@gmail.com>
2024-08-01 13:36:01 +0300

    implemented useRouterError() hook in the error page for additional error info

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -1,11 +1,17 @@
+import { useRouteError } from "react-router-dom";
 import NavigationBar from "../Navigation/NavigationBar";
 
 export default function Error() {
+    const error = useRouteError();
+    console.log(error.data);
     return (
         <>
             <NavigationBar />
             <div className="page-bg authpage-bg"></div>
-            <h1 className="error-title">An error has occured!</h1>
+            <div className="error-page">
+                <h1>An error has occured!</h1>
+                <p>{error.data || ""}</p>
+            </div>
         </>
     );
 }
